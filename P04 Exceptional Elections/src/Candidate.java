4523ec747af11e8d16ad9b52bb980fb9de5012a2
mnanthak <mnanthakumar@wisc.edu>
2024-10-04 21:50:35 -0500

    adding file headers

--- P04 Exceptional Elections/src/Candidate.java
+++ P04 Exceptional Elections/src/Candidate.java
@@ -1,3 +1,24 @@
+//////////////// FILE HEADER //////////////////////////
+//
+// Title:    The ElectionManagerTester Class contains tester methods for the methods of the Candidate, Ballot, and Election classes
+// Course:   CS 300 Fall 2024
+//
+// Author:   Mohnsih Nanthakumar
+// Email:    mnanthakumar@wisc.edu
+// Lecturer: Hobbes LeGault
+//
+//////////////////// PAIR PROGRAMMERS ///////////////////
+// 
+// Partner Name:    Harsh Singh
+// Partner Email:   hvsingh@wisc.edu
+// Partner Lecturer's Name: Hobbes LeGault
+// 
+// VERIFY THE FOLLOWING BY PLACING AN X NEXT TO EACH TRUE STATEMENT:
+//   X Write-up states that pair programming is allowed for this assignment.
+//   X We have both read and understand the course Pair Programming Policy.
+//   X We have registered our team prior to the team registration deadline.
+//
+///////////////////////////////////////////////////////////////////////////////
 
 public class Candidate {
 
